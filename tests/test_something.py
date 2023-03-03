83254b08084cde32a165a33fdcba1202fae8a89c
User_Hinter <lovewaxbaby@gmail.com>
2023-03-03 23:30:25 +0200

    Initial commit

---
+++ tests/test_something.py
@@ -0,0 +1,13 @@
+import requests
+
+# from src.schemas.post import POST_SCHEMA
+from src.baseclasses.response import Response
+from configuration import SERVICE_URL
+
+from src.pydantic_schemas.post import Post
+
+def test_getting_posts():
+    r = requests.get(url=SERVICE_URL)
+    response = Response(r)
+
+    response.assert_status_code(200).validate(Post)
